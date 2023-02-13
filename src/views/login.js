0daeee6ce7f07921db3ef45a0379c0618cc134dd
Mateus Paz <mateuspaz.mp@gmail.com>
2023-02-13 12:28:41 -0300

    Primeiras rotas e componentes de usuário e login implementados

--- src/views/login.js
+++ src/views/login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
+import { withRouter } from 'react-router-dom'
 
 class Login extends React.Component{
 
@@ -14,47 +15,53 @@ class Login extends React.Component{
         console.log('Senha: ', this.state.senha)
     }
 
+    prepareCadastrar = () => {
+        this.props.history.push('/cadastro-usuarios')
+    }
+
     render(){
         return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6" style={ { position:'relative', left:'300px' } }>
-                        <div className="bs-docs-section">
-                            <Card title="Login">
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                        <div className="bs-component">
-                                            <fieldset>
-                                                <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
-                                                    <input type="email" 
-                                                            value={this.state.email}
-                                                            onChange={ e => this.setState({email: e.target.value})}
-                                                            className="form-control" 
-                                                            id="exampleInputEmail1" 
-                                                            aria-describedby="emaliHelp" placeholder="Digite o Email"/>
-                                                </FormGroup>
+        
+            <div className="row">
+                <div className="col-md-6" style={ {position:'relative', left:'300px'} }>
+                    <div className="bs-docs-section">
+                        <Card title="Login">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                            <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                                                <input type="email" 
+                                                        value={this.state.email}
+                                                        onChange={ e => this.setState({email: e.target.value})}
+                                                        className="form-control" 
+                                                        id="exampleInputEmail1" 
+                                                        aria-describedby="emaliHelp" 
+                                                        placeholder="Digite o Email"/>
+                                            </FormGroup>
 
-                                                <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
-                                                    <input type="password" 
-                                                            value={this.state.senha}
-                                                            onChange={ e => this.setState({senha: e.target.value})}
-                                                            className="form-control" 
-                                                            id="exampleInputPassword1" 
-                                                            aria-describedby="emaliHelp" placeholder="Password"/>
-                                                </FormGroup>
-                                                <button className="btn btn-success" onClick={this.entrar}>Entrar</button>
-                                                <button className="btn btn-danger">Cadastrar</button>
-                                            </fieldset>
-                                        </div>
+                                            <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                <input type="password" 
+                                                        value={this.state.senha}
+                                                        onChange={ e => this.setState({senha: e.target.value})}
+                                                        className="form-control" 
+                                                        id="exampleInputPassword1" 
+                                                        aria-describedby="emaliHelp" 
+                                                        placeholder="Password"/>
+                                            </FormGroup>
+                                            <button onClick={this.entrar} type="button" className="btn btn-success">Entrar</button>
+                                            <button onClick={this.prepareCadastrar} type="button" className="btn btn-danger">Cadastrar</button>
+                                        </fieldset>
                                     </div>
                                 </div>
-                            </Card>
-                        </div>
+                            </div>
+                        </Card>
                     </div>
                 </div>
             </div>
+                
         )
     }
 }
 
-export default Login
+export default withRouter( Login )
